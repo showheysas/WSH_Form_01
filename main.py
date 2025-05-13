ce352762c99c54d5f5f15cc7e0fd9d8b432bf5a3
showheysas <shoheysas@gmail.com>
2025-05-14 00:55:22 +0900

    API参照変更

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import seaborn as sns
 import matplotlib.font_manager as fm
 import os
 import re
+import json
+from google.oauth2.service_account import Credentials
 
 # 日本語フォント指定
 def get_japanese_font():
@@ -65,15 +67,16 @@ html, body, [class*="css"] {
 </style>
 """, unsafe_allow_html=True)
 
-# Google Sheets API
+# Google Sheets API認証
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds_path = "google-credentials.json"
-if os.path.exists(creds_path):
-    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scope)
+
+if "GOOGLE_SERVICE_ACCOUNT_JSON" in st.secrets:
+    service_account_info = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT_JSON"])
+    creds = Credentials.from_service_account_info(service_account_info, scopes=scope)
     client = gspread.authorize(creds)
     sheet = client.open("202505-WSH-Form").sheet1
 else:
-    st.error("Google認証ファイルが見つかりません。google-credentials.json を配置してください。")
+    st.error("Google認証情報が見つかりません。Secretsを設定してください。")
     sheet = None
 
 st.title("週末共有会アンケート！")
